796eb8c06301e4da3eaca30f25d423904e3c03b8
DWan-Dall <dwandall@gmail.com>
2024-04-05 12:18:16 -0300

    Adicionar arquivos de projeto.

---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,33 @@
+﻿using PassIn.Communication.Requests;
+using PassIn.Exceptions;
+
+namespace PassIn.Application.UseCases.Events.Register
+{
+    public class RegisterEventUseCase
+    {
+        public void Execute(RequestEventJson request)
+        {
+            Validate(request);
+        }
+
+        private void Validate(RequestEventJson request)
+        {
+            if(request.MaximumAttendees <= 0)
+            {
+                throw new PassInException("O número de pessoas é inválido.");
+            }
+
+            if(string.IsNullOrWhiteSpace(request.Title))
+            {
+                throw new PassInException("O título é inválido.");
+            }
+
+            if(string.IsNullOrWhiteSpace(request.Details))
+            {
+                throw new PassInException("A descrição é inválida.");
+            }
+
+
+        }
+    }
+}
